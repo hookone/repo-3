ef570c02919a26c453a75920e50d5c37087551d3
hookone <52318234+hookone@users.noreply.github.com>
2026-08-12 14:52:13 +0800

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -3,6 +3,7 @@ public class UserController{
   @GetMapping("/hello")
   public String hello(){
     System.out.prinln("userCOntrller的hello方法执行了");
+    Systme.out.println("ssf");
     return "hello git!";
   }
 }
